7e97bbd1fbad39bf1b9daa133a9e87027cb72a6a
bo-yuan026 <scsa8860512@gmail.com>
2021-07-31 13:21:52 +0800

    basic file

---
+++ basic/sum.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+function sum(n){
+    let result = 0;
+
+    for (i = 1; i<=n ; i++){
+        result += i;
+        
+    } //for 
+    return result;
+}
+
+console.log(sum(1)); //
+console.log(sum(2)); // 3

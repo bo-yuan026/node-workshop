2111b74570dc3221a1e4cabea4419544ddbe3829
bo-yuan026 <scsa8860512@gmail.com>
2021-08-07 20:42:15 +0800

    使用promise方式

---
+++ crawler/crawler2.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+const moment = require("moment");
+const fs = require("fs");
+
+let today = moment().format("YYYYMMDD");
+
+// let stockNumber = fs.readFileSync("stock.txt", "utf8");
+
+new Promise((resolve, reject) => {
+  //   fs.readFile("stock.txt", "utf8", (err, stockNumber) => {
+  //     if (err) {
+  //       //   console.error(err);
+  //       reject(err);
+  //     } else {
+  //       //   console.log(stockNumber);
+  //       resolve(stockNumber);
+  //     }
+  //   });
+  let stockNumber = fs.readFileSync("stock.txt", "utf8");
+  if (stockNumber !== null) {
+    resolve(stockNumber);
+  } else {
+    reject(`出錯了`);
+  }
+}).then((stockNumber) => {
+  axios
+    .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      params: {
+        response: "json",
+        date: today,
+        stockNo: stockNumber,
+      },
+    })
+    .then(function (response) {
+      // handle success
+      console.log(response.data.title);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+});

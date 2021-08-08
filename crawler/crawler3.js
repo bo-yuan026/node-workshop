381d33c0bebe45ddf6538c1591404bd7e0381640
bo-yuan026 <scsa8860512@gmail.com>
2021-08-08 10:47:01 +0800

    重新上傳async/await 方式

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -6,32 +6,61 @@ let today = moment().format("YYYYMMDD");
 
 // let stockNumber = fs.readFileSync("stock.txt", "utf8");
 
-async function f() {
-  await new Promise((resolve, reject) => {
-    let stockNumber = fs.readFileSync("stock.txt", "utf8");
-    if (stockNumber !== null) {
-      resolve(stockNumber);
-    } else {
-      reject(`出錯了`);
-    }
-  }).then((stockNumber) => {
-    axios
-      .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-        params: {
-          response: "json",
-          date: today,
-          stockNo: stockNumber,
-        },
-      })
-      .then(function (response) {
-        // handle success
-        console.log(response.data.title);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      });
+// function resultP() {
+//   return new Promise((resolve, reject) => {
+//     let stockNumber = fs.readFileSync("stock.txt", "utf8");
+//     if (stockNumber !== null) {
+//       resolve(stockNumber);
+//     } else {
+//       reject(`出錯了`);
+//     }
+//   });
+// }
+
+// function resultS(stockNumber) {
+//   return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+//     params: {
+//       response: "json",
+//       date: today,
+//       stockNo: stockNumber,
+//     },
+//   });
+// }
+
+// async function f() {
+//   try {
+//     const readFile = await resultP();
+//     const stock = await resultS(readFile);
+//     console.log(stock.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// f();
+
+// --------------------------------------
+
+async function g() {
+  let stockNumber = await new Promise((resolve, reject) => {
+    fs.readFile("stock.txt", "utf8", (err, stockNumber) => {
+      if (err) {
+        //   console.error(err);
+        reject(err);
+      } else {
+        // console.log(stockNumber);
+        resolve(stockNumber);
+      }
+    });
   });
+  let response = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+    params: {
+      response: "json",
+      date: today,
+      stockNo: stockNumber,
+    },
+  });
+  console.log(response.data);
 }
 
-f();
+g();

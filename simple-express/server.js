2b63a322d0b240986bd74e9f1c0f25177d248ea6
bo-yuan026 <scsa8860512@gmail.com>
2021-08-15 11:33:13 +0800

    測試兩個中間件

--- simple-express/server.js
+++ simple-express/server.js
@@ -1,3 +1,4 @@
+const { response, request } = require("express");
 const express = require("express");
 
 let app = express();
@@ -6,9 +7,25 @@ app.listen(3000, () => {
   console.log("web server啟動!!");
 });
 
+app.use((request, response, next) => {
+  console.log("我是第一個中間件");
+  next();
+});
+
+app.use((request, response, next) => {
+  let currentTime = new Date();
+  console.log(`我是第二個中間件: ${currentTime}`);
+  next();
+});
+
 app.get("/", (request, response, next) => {
   response.send("Hello 這是首頁");
 });
+
 app.get("/about", (request, response, next) => {
   response.send("關於我們");
+});
+
+app.get("/abc", (request, response, next) => {
+  response.send("被你發現abc了");
 });
